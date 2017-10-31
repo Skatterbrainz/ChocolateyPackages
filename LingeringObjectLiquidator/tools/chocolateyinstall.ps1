e5aa79d00664463e1bcb7c7f3a6eddd1b1eb0676
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-10-30 23:43:46 -0400

    Add files via upload

--- LingeringObjectLiquidator/tools/chocolateyinstall.ps1
+++ LingeringObjectLiquidator/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'; 
+﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidator.msi' 
 $url64      = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidator.msi' 
@@ -11,7 +11,7 @@ $packageArgs = @{
   url64bit      = $url64
   softwareName  = 'Lingering Object Liquidator*' 
   checksum      = 'D68D6438A2558DEFEF348FBD51A70D11DD1A885B8DD8B4FF8C3963D2496E12C6'
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
   checksum64    = 'D68D6438A2558DEFEF348FBD51A70D11DD1A885B8DD8B4FF8C3963D2496E12C6'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
