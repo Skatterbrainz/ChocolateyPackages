0fd62dada70ee9e9d8324642c22ca5cceaa0510b
skatterbrainz <dstein2017@gmail.com>
2019-10-29 02:39:00 -0400

    new package

--- office-readiness-toolkit/tools/chocolateyinstall.ps1
+++ office-readiness-toolkit/tools/chocolateyinstall.ps1
@@ -1,67 +1,17 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-# 3. In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
-$url        = 'https://download.microsoft.com/download/E/C/7/EC7CDC27-3750-4F12-8DB4-ABFDFDF7DA5C/ReadinessToolkitForOffice_1.2.19231.msi' # download url, HTTPS preferred
-#$url64      = ''
+$url        = 'https://download.microsoft.com/download/E/C/7/EC7CDC27-3750-4F12-8DB4-ABFDFDF7DA5C/ReadinessToolkitForOffice_1.2.19231.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI' #only one of these: exe, msi, msu
+  fileType      = 'MSI'
   url           = $url
-  #url64bit      = $url64
-  #file         = $fileLocation
-
-  softwareName  = 'office-readiness-toolkit*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  # Checksums are now required as of 0.10.0.
-  # To determine checksums, you can get that from the original site if provided. 
-  # You can also use checksum.exe (choco install checksum) and use it 
-  # e.g. checksum -t sha256 -f path\to\file
+  softwareName  = 'Readiness Toolkit for Office Add-ins and VBA' 
   checksum      = 'CA23818C79AA66EC636A2887E008AFBCA0808C19F0C8468A7470CA0E43193357'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  #checksum64    = ''
-  #checksumType64= 'sha256' #default is checksumType
-
-  # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
-
-## Install an application, will assert administrative rights
-## - https://chocolatey.org/docs/helpers-install-chocolatey-package
-## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-## add additional optional arguments as necessary
-##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
-
-Get-ChocolateyWebFile $packageName 'DOWNLOAD_TO_FILE_FULL_PATH' $url
-
-## Installer, will assert administrative rights - used by Install-ChocolateyPackage
-## use this for embedding installers in the package when not going to community feed or when you have distribution rights
-## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#Install-ChocolateyInstallPackage $packageName $fileType $silentArgs '_FULLFILEPATH_' -validExitCodes $validExitCodes
-
-## Outputs the bitness of the OS (either "32" or "64")
-## - https://chocolatey.org/docs/helpers-get-o-s-architecture-width
-#$osBitness = Get-ProcessorBits
+Install-ChocolateyPackage @packageArgs 
