b9cd8ebd5010f439913aefaffe1a801889ccad44
skatterbrainz <dstein2017@gmail.com>
2020-06-05 19:05:06 -0400

    updated for 2.0.0.2

--- wmiexplorer/tools/chocolateyinstall.ps1
+++ wmiexplorer/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ Write-Host "WMI Explorer is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName   = 'wmiexplorer'
   url           = 'https://github.com/vinaypamnani/wmie2/releases/download/v2.0.0.2/WmiExplorer_2.0.0.2.zip'
-  checksum      = '343695AE7BCD048DA51EBE0949BDF746F7C16F648746D9A4554B2CB888007851'
+  checksum      = '64B5BBCF609F86383B7CAF1FF8C4562D1362BA694A511F1C25BF50349979AA73'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   unzipLocation = $installDir
 }
