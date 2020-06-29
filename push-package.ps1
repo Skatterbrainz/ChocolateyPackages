fcff5100921d61a06e1d1ef2971f5cd6805be50b
skatterbrainz <dstein2017@gmail.com>
2020-06-28 23:57:57 -0400

    fixed timeout param

--- push-package.ps1
+++ push-package.ps1
@@ -4,7 +4,7 @@ param (
 	[parameter()][string] $ApiKey = '798f13de-c87c-44aa-987e-8acac023a3a1'
 )
 try {
-	choco push $NuPkgPath --source 'https://chocolatey.org/' -k="$ApiKey" -t 500
+	choco push $NuPkgPath --source 'https://chocolatey.org/' -k="$ApiKey" --timeout=500
 }
 catch {
 	Write-Error $_.Exception.Message
