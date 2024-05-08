317cef0dc826087dafb549844d25a5cf81feb81d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-05-08 14:06:25 -0400

    2.0.21

--- LingeringObjectLiquidator/tools/chocolateyinstall.ps1
+++ LingeringObjectLiquidator/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidator.msi' 
-$url64      = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidator.msi' 
+$url        = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidatorInstaller.msi'
+$url64      = 'https://download.microsoft.com/download/B/A/A/BAA58D37-F8D9-4A92-8321-15CAB1DEFF51/LingeringObjectLiquidatorInstaller.msi' 
 
 $packageArgs = @{
 	packageName   = $env:ChocolateyPackageName
@@ -10,9 +10,9 @@ $packageArgs = @{
 	url           = $url
 	url64bit      = $url64
 	softwareName  = 'Lingering Object Liquidator*' 
-	checksum      = 'D68D6438A2558DEFEF348FBD51A70D11DD1A885B8DD8B4FF8C3963D2496E12C6'
+	checksum      = '8B69CD3E64AA08FA3688109C861EAF8534F46CF569633C69FEDCA5048BE6FF01'
 	checksumType  = 'sha256'
-	checksum64    = 'D68D6438A2558DEFEF348FBD51A70D11DD1A885B8DD8B4FF8C3963D2496E12C6'
+	checksum64    = '8B69CD3E64AA08FA3688109C861EAF8534F46CF569633C69FEDCA5048BE6FF01'
 	checksumType64= 'sha256'
 	silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
 	validExitCodes= @(0, 3010, 1641)
